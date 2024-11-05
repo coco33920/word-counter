88035f6c5a6a7b2be147d9cb0583ca7292227112
Charlotte Thomas <contact@nwa2coco.fr>
2024-11-05 17:34:21 +0100

    WIP counting

--- src/commands/commands.rs
+++ src/commands/commands.rs
@@ -1,6 +1,6 @@
-use poise::{samples::HelpConfiguration, serenity_prelude::Error, Context};
+use poise::{samples::HelpConfiguration, serenity_prelude::{Error, GetMessages, Message, User}, Context};
 
-use crate::Data;
+use crate::{utils::utils, Data};
 
 #[poise::command(prefix_command, slash_command)]
 pub async fn help(
@@ -18,5 +18,35 @@ pub async fn help(
         },
     )
     .await?;
+    Ok(())
+}
+
+#[poise::command(prefix_command,slash_command)]
+pub async fn count_user_words(
+    ctx: Context<'_, Data, Error>,
+    #[description = "selected user"] user: Option<User>
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let channels = ctx.guild().unwrap().channels(ctx.http()).await?;
+    let ignored_channels = vec!["".to_string()];
+    let mut total_words = 0;
+    let mut total_messages = Vec::new();
+    for (id,channel) in channels {
+        if ignored_channels.contains(&id.to_string()) {
+            continue;
+        }
+    
+        let message = channel.messages(ctx.http(), GetMessages::new().limit(1)).await?;
+        let mut co = if message.len() == 1 {Some(message.get(0).unwrap())} else {None};
+        
+        while let Some(message) = co  {
+            let messages = channel.messages(ctx.http(), GetMessages::new().before(message.id).limit(100)).await?;
+            messages.clone().iter().for_each(|x: &Message| {if u.id == x.author.id {total_messages.push(x)}});
+            co = if messages.clone().len() > 0 {Some(messages.clone().get(messages.clone().len() - 1).unwrap())} else {None}
+        }
+    }
+    total_messages.clone().into_iter().for_each(|f| {total_words += utils::count_words(f.clone().content)});
+
+    ctx.say(format!("{} wrote {} words since they arrived",u.name,total_words)).await?;
     Ok(())
 }
